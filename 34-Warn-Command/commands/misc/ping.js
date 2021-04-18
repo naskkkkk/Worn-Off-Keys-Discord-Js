52c76318dcaf8fd7d5232f9bd26a74fb743b8494
SgyMAZSE5ZBuHgg <>
2021-04-18 22:05:54 +0000

    Teste

--- 34-Warn-Command/commands/misc/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  commands: 'ping',
-  minArgs: 0,
-  maxArgs: 0,
-  description: 'Replies with pong',
-  callback: (message, arguments, text) => {
-    message.reply('Pong!')
-  },
-}
