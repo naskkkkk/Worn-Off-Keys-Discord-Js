428fc798971a9fc1c3023992017511c77a0529a6
naskkkkk <82781862+naskkkkk@users.noreply.github.com>
2021-04-18 18:50:27 -0300

    Delete 01-Creating-The-Bot directory

--- 01-Creating-The-Bot/index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const config = require('./config.json')
-
-client.on('ready', () => {
-  console.log('The client is ready!')
-})
-
-client.login(config.token)
